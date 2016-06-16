2fbd846b2ba419b66e7a16f831401a7b8fdf5869
Angela Xu <xuhuanzhu@gmail.com>
2016-06-16 13:00:03 -0700

    Add .py in file name.

---
+++ PygLatinGameTranslator.py
@@ -0,0 +1,16 @@
+"""
+Pig Latin is a language game, where you move the first letter of the word to the end and add "ay". 
+So "Python" becomes "ythonpay".
+"""
+
+print("Welcome to Pig Latin Game")
+pyg = 'ay'
+word = raw_input("Please enter a word:")     # ask the user to enter a word
+
+if len(word) > 0 and word.isalpha():         # check if the word has and only has characters
+    word = word.lower()
+    first = word[0]
+    new_word = word[1:] + first + pyg
+    print(new_word)
+else:
+    print('invalid word')
